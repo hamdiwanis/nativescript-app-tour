cc0166b53e019b88bbbe5301fd53a34638a5f9f9
hamdiwanis <hamdiwanis@hotmail.com>
2018-07-12 15:55:40 +0200

    remove publish workaround as fixed by npm 6

--- publish/publish.sh
+++ publish/publish.sh
@@ -5,7 +5,7 @@ PACK_DIR=package;
 publish() {
     cd $PACK_DIR
     echo 'Publishing to npm...'
-    node_modules/.bin/npm publish package/*.tgz
+    npm publish package/*.tgz
 }
 
 ./pack.sh && publish
